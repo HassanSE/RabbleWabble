016817556a7745e579bf2cc9837b7e8e33dc982f
Muhammad Hassan <hassankhll@hotmail.com>
2020-10-24 21:10:36 +0500

    - Add new groups and restructure project hierarchy - Add QuestionViewController.swift

--- RabbleWabble/Controllers/QuestionViewController.swift
+++ RabbleWabble/Controllers/QuestionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QuestionViewController.swift
 //  RabbleWabble
 //
 //  Created by Muhammad Hassan on 24/10/2020.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QuestionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
